1e922429ed56d99b796ca0b04f54682c804a9168
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-15 09:03:51 +0300

    Update models with database changes

--- app/models/budget_transaction.rb
+++ app/models/budget_transaction.rb
@@ -1,6 +1,6 @@
-class Transaction < ApplicationRecord
+class BudgetTransaction < ApplicationRecord
     belongs_to :user, foreign_key: 'user_id'
-    has_and_belongs_to_many :categories, foreign_key: 'transaction_id'
+    has_and_belongs_to_many :categories, foreign_key: 'budget_transaction_id'
 
     validates :name, presence: true, length: { maximum: 250 }
     validates :amount, presence: true, numericality: { only_integer: true }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :categories, foreign_key: 'user_id'
-    has_many :transactions, foreign_key: 'user_id'
+    has_many :budget_transactions, foreign_key: 'user_id'
 
     validates :name, presence: true, length: { minimum: 3, maximum: 250 }
 end
