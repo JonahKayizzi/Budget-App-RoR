b76d3e68b146062b353e5a925338f6172378ea2c
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-15 09:04:38 +0300

    Add rspec tests for users, categories and budget transactions

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+    before :each do
+        @user = User.create(name: 'Microverse')
+        @category = Category.create(name:'Microverse', icon:'microverser.png', user: @user)
+    end
+    
+    context 'validation tests' do
+        it 'is valid with attributes' do
+            expect(@category).to be_valid
+        end
+
+        it 'name must be present' do
+            @category.name = nil
+            expect(@category).to_not be_valid
+        end
+
+        it 'icon must be present' do
+            @category.icon = nil
+            expect(@category).to_not be_valid
+        end
+    end
+end
---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe BudgetTransaction, type: :model do
+    before :each do
+        @user = User.create(name: 'Microverse')
+
+        @category = Category.create(name:'Microverse', icon:'microverser.png', user: @user)
+
+        @transaction = BudgetTransaction.create(name:'Microverse Transaction', amount: 100, user: @user)
+
+        @transaction.categories << @category
+    end
+
+    context 'validation tests' do
+        it 'is valid with attributes' do
+            expect(@transaction).to be_valid
+        end
+
+        it 'name must be present' do
+            @transaction.name = nil
+            expect(@transaction).to_not be_valid
+        end
+
+        it 'amount must be present' do
+            @transaction.amount = nil
+            expect(@transaction).to_not be_valid
+        end
+
+        it 'category must be present' do
+            @transaction.categories = []
+            expect(@transaction).to_not be_valid
+        end
+    end
+end
