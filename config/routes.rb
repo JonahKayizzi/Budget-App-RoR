832e8cbdfc109e6e95ab4506fe763c34e8558d2a
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-15 16:47:58 +0300

    Config app routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
   root 'categories#index'
+  resources :categories, only: [:index, :new, :create, :show] do
+    resources :transactions, only: [:index, :new, :create]
+  end
 end
